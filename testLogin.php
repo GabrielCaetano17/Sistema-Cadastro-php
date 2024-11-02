5b7bd182b11b67fd05841df0821ffb1b6a079ccc
Gabriel Caetano <128912687+GabrielCaetano17@users.noreply.github.com>
2024-11-02 17:01:06 -0300

    Update testLogin.php

--- testLogin.php
+++ testLogin.php
@@ -1,23 +1,23 @@
 <?php
     session_start();
-    // print_r($_REQUEST);
+     print_r($_REQUEST);
     if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
     {
-        // Acessa
+   
         include_once('config.php');
         $email = $_POST['email'];
         $senha = $_POST['senha'];
 
-        // print_r('Email: ' . $email);
-        // print_r('<br>');
-        // print_r('Senha: ' . $senha);
+         print_r('Email: ' . $email);
+         print_r('<br>');
+         print_r('Senha: ' . $senha);
 
         $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
 
         $result = $conexao->query($sql);
 
-        // print_r($sql);
-        // print_r($result);
+         print_r($sql);
+         print_r($result);
 
         if(mysqli_num_rows($result) < 1)
         {
@@ -34,7 +34,7 @@
     }
     else
     {
-        // Não acessa
+
         header('Location: login.php');
     }
 ?>
